889a5935cc49a060d01669f21685bf28a45522de
Yanjinux <471573617@qq.com>
2018-06-06 00:07:53 +0800

    commit common.h

---
+++ include/common.h
@@ -0,0 +1,13 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define err_log(fmt,str) printf(fmt,str)
+#define info_log(fmt,str) printf(fmt,str)
+#define dbg_log(fmt,str) printf(fmt,str)
+
+
+#endif
